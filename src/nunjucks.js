900c2a694b6b80ee1554945aeb0e26704be19845
Igor Spasić <igor@jodd.org>
2019-05-16 22:39:25 +0200

    updated to sping v1.5.3

--- src/nunjucks.js
+++ src/nunjucks.js
@@ -1,26 +1,9 @@
 "use strict";
 
-const datetimefmt = require('../spig/filters/datetimefmt');
-const pages = require('../spig/filters/pages');
-
 const rs = require('./filters/rs');
 
 module.exports = nunjucksEnv => {
   nunjucksEnv
-    .addFilter('out', require('../spig/filters/out'))
-
-    .addFilter('dateDisplay', datetimefmt.dateDisplay)
-    .addFilter('dateISO', datetimefmt.dateISO)
-    .addFilter('dateUTC', datetimefmt.dateUTC)
-
-    .addFilter('within', pages.within)
-    .addFilter('reverse', pages.reverse)
-    .addFilter('sortBy', pages.sortBy)
-    .addFilter('groupBy', pages.groupBy)
-    .addFilter('groupByYear', pages.groupByDateYear)
-    .addFilter('lastN', pages.lastN)
-    .addFilter('firstN', pages.firstN)
-
     .addFilter('rsDateShort', rs.dateShort)
     .addFilter('rsDateLong', rs.dateLong)
 
