049319959d153ebab7d8f7c7d191f5e437246294
Igor Spasić <igor.spasic@gmail.com>
2024-06-12 23:41:16 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from "@astrojs/mdx";
+import sitemap from '@astrojs/sitemap';
 import remarkFlexibleContainers from 'remark-flexible-containers';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://oblac.rs',
   integrations: [
-    mdx()
+    mdx(),
+    sitemap(),
   ],
   markdown: {
     remarkPlugins: [
