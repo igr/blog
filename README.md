d009a79976ab9397b8600d020c1b32a248b211b7
Igor Spasić <igor.spasic@gmail.com>
2024-11-03 12:04:23 +0100

    Add pagefind

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 [![Netlify Status](https://api.netlify.com/api/v1/badges/ba8ba248-cb24-4196-b030-e7950bb0ebde/deploy-status)](https://app.netlify.com/sites/oblac-rs/deploys)
 
 🔗 [oblac.rs](https://oblac.rs)
+
+## Development notes
+
++ the `pagefinder` integration is added manually. The site needs to be rebuild first to have the search index created and updated.
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import sitemap from '@astrojs/sitemap';
 import remarkFlexibleContainers from 'remark-flexible-containers';
+import {pagefind} from "./src/integrations/pagefind.ts";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +11,7 @@ export default defineConfig({
   integrations: [
     mdx(),
     sitemap(),
+    pagefind()
   ],
   markdown: {
     remarkPlugins: [
